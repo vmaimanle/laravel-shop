4a6df6d5fb1166b4c8cb89e5507e445493d57b61
vagrant <vagrant@homestead>
2019-12-25 12:37:04 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -15,5 +14,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    Route::post('cart', 'CartController@add')->name('cart.add');
 
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
